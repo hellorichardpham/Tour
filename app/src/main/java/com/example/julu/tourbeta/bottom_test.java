9954ff199f0c7cc03523225d504891a77602ff8d
Dylan Tieu <dylantieu@eduroam-169-233-205-162.ucsc.edu>
2017-02-08 12:15:26 -0800

    First Commit

---
+++ app/src/main/java/com/example/julu/tourbeta/bottom_test.java
@@ -0,0 +1,18 @@
+package com.example.julu.tourbeta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dylantieu on 2/3/17.
+ */
+
+public class bottom_test extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_test);
+        System.out.println("Hello i've been created");
+    }
+}
