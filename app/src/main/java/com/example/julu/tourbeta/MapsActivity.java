8a02062782ada73e7247718488e0b42da76b44b9
Richard Pham <dylantieu@eduroam-169-233-225-147.ucsc.edu>
2017-02-23 23:10:22 -0800

    Working filter button

--- app/src/main/java/com/example/julu/tourbeta/MapsActivity.java
+++ app/src/main/java/com/example/julu/tourbeta/MapsActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -46,6 +47,7 @@ import com.ms.square.android.expandabletextview.ExpandableTextView;
 import java.util.ArrayList;
 
 import static android.R.attr.id;
+import static android.R.attr.tag;
 import static com.example.julu.tourbeta.R.drawable.be105;
 import static com.example.julu.tourbeta.R.id.bottomsheet;
 import static com.example.julu.tourbeta.R.id.bottomtest;
@@ -62,7 +64,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private SQLiteDatabase myDB = null;
     private String TableName = "testdb4";
     private ExpandableTextView expandableText;
+    private String[] majorArray = {"Computer Science", "CS: Game Design",
+            "Computer Engineering", "Electrical Engineering",
+            "Technology Information Management", "Bioengineering"};
 
+    private ArrayList<String> majorsToDisplay = new ArrayList<>();
+    private FloatingActionsMenu rightLabels;
+    private ArrayList<Marker> listOfMarkers = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,43 +78,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         setupBottomSheet();
         setupDatabase();
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
 
         // Getting LocationManager object from System Service LOCATION_SERVICE
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-/*        View parentView = bs.getSheetView();
 
-        FloatingActionsMenu rightLabels = (FloatingActionsMenu) parentView.findViewById(R.id.multiple_layout)
-                .findViewById(R.id.multiple_actions);
-        FloatingActionButton addedOnce = new FloatingActionButton(this);
-        addedOnce.setTitle("Computer Science");
-        rightLabels.addButton(addedOnce);
+        rightLabels = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
 
-        FloatingActionButton addedTwice = new FloatingActionButton(this);
-        addedTwice.setTitle("Computer Engineering");
-        rightLabels.addButton(addedTwice);
-        rightLabels.removeButton(addedTwice);
-        rightLabels.addButton(addedTwice);
-*/
+        setupButtons();
 
+
+
+
+    }
+    /*
+
+        private String[] majorArray = {"Computer Science", "Computer Science: GD",
+            "Computer Engineering", "Electrical Engineering",
+            "Technology Information Management", "Bioengineering"};
+     */
+
+    //go through all majorArray where each index is contained within majorsToDisplay
+
+    public void addAllMajors() {
+        for(int i = 0; i < majorArray.length; i++) {
+            //majorsToDisplay.add(majorArray[i]);
+        }
+    }
+
+    public void initializeChosenMajorsToDisplay() {
+        mMap.clear();
+        for(int i = 0; i < majorArray.length; i++) {
+
+            String major = majorArray[i];
+
+            if(majorsToDisplay.contains(major)) {
+                System.out.println("We will display " + major);
+                generateMarkersFromDatabaseUsingMajor(major);
+            }
+        }
+    }
+
+    public void setupButtons() {
+        for(int i=0;i<majorArray.length;i++) {
+
+            FloatingActionButton fab = new FloatingActionButton(this);
+            fab.setTitle(majorArray[i]);
+            fab.setSize(FloatingActionButton.SIZE_MINI);
+            final String major = fab.getTitle();
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(majorsToDisplay.contains(major)) {
+                        System.out.println(major + " is contained so I will remove it now");
+                        majorsToDisplay.remove(major);
+                    } else {
+                        System.out.println(major + " is not here so i will add it now");
+                        majorsToDisplay.add(major);
+                    }
+                    initializeChosenMajorsToDisplay();
+                }
+            });
+            rightLabels.addButton(fab);
+        }
     }
 
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng defaultPoint = new LatLng(37.000225, -122.063148);
-        generateMarkersFromDatabase();
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom((defaultPoint), 18.01f));
-
-        mMap.setOnMarkerClickListener(this);
-
-    }
 
     /*
         roomInformation[0] = c.getString(idIndex);
@@ -163,23 +205,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // IMPORTANT - call setText on the ExpandableTextView to set the text content to display
         expandableText.setText(roomInformation[5]);
-        // Return false to indicate that we have not consumed the event and that we wish
-        // for the default behavior to occur (which is for the camera to move such that the
-        // marker is centered and for the marker's info window to open, if it has one).
 
-/*
-        FloatingActionsMenu rightLabels = (FloatingActionsMenu) parentView.findViewById(R.id.multiple_layout)
-                .findViewById(R.id.multiple_actions);
-        FloatingActionButton addedOnce = new FloatingActionButton(this);
-        addedOnce.setTitle("Computer Science");
-        rightLabels.addButton(addedOnce);
 
-        FloatingActionButton addedTwice = new FloatingActionButton(this);
-        addedTwice.setTitle("Computer Engineering");
-        rightLabels.addButton(addedTwice);
-        rightLabels.removeButton(addedTwice);
-        rightLabels.addButton(addedTwice);
-*/
+
         return false;
     }
 
@@ -202,7 +230,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             myDB.execSQL("INSERT INTO "
                     + TableName
                     + " (id, title, location, major, image, description, latitude, longitude)"
-                    + " VALUES (1,'BE 368: Game Design Lab', 'Baskin Engineering 1 Room 368', 'Computer Science Game Design', " +
+                    + " VALUES (1,'BE 368: Game Design Lab', 'Baskin Engineering 1 Room 368', 'CS: Game Design', " +
                     "'defaultimage'," +
                     " 'The Game Lab is among the most distinctive teaching labs at UC Santa Cruz. Here, undergraduate game majors work in teams of 4 to 15 students to create a substantial computer game over their entire senior year. " +
                     "\n\nGame Lab games have won awards at game festivals such as IndieCade and the Google Play Indie Games awards, and are publicly released on platforms such as Itch.io, the Apple App Store, and the Google Play Store. " +
@@ -260,7 +288,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    public void generateMarkersFromDatabase() {
+
+
+    public void generateMarkersFromDatabaseUsingMajor(String requestedMajor) {
+        Cursor c = myDB.rawQuery("SELECT * FROM " + TableName + " WHERE  major = " + "'"+requestedMajor+"'", null);
+
+        float colorOfMarker;
+        while (c.moveToNext()) {
+            int idIndex = c.getColumnIndex("id");
+            int titleIndex = c.getColumnIndex("title");
+            int latitudeIndex = c.getColumnIndex("latitude");
+            int longitudeIndex = c.getColumnIndex("longitude");
+            int majorIndex = c.getColumnIndex("major");
+
+            String id = c.getString(idIndex);
+            String title = c.getString(titleIndex);
+            String latitude = c.getString(latitudeIndex);
+            String longitude = c.getString(longitudeIndex);
+            String major = c.getString(majorIndex);
+            colorOfMarker = getMarkerColor(major);
+
+            int tag = Integer.parseInt(id);
+            double lat = Double.parseDouble(latitude);
+            double lon = Double.parseDouble(longitude);
+            LatLng latLong = new LatLng(lat, lon);
+
+
+            Marker marker = mMap.addMarker(new MarkerOptions().position(latLong).title(title).icon(BitmapDescriptorFactory.defaultMarker(colorOfMarker)));
+            marker.setTag(tag);
+
+            listOfMarkers.add(marker);
+        }
+    }
+
+        public void generateMarkersFromDatabase() {
 
         /*
             Computer Sci - DEFAULT
@@ -278,8 +339,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         Cursor c = myDB.rawQuery("SELECT * FROM " + TableName, null);
-        String[] roomInformation = new String[c.getColumnCount()];
-        ArrayList<Marker> listOfMarkers = new ArrayList<>();
+
         float colorOfMarker;
         while (c.moveToNext()) {
             int idIndex = c.getColumnIndex("id");
@@ -380,4 +440,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         bs.setPeekSheetTranslation(px);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng defaultPoint = new LatLng(37.000225, -122.063148);
+        addAllMajors();
+        initializeChosenMajorsToDisplay();
+        //generateMarkersFromDatabase();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom((defaultPoint), 18.01f));
+
+        mMap.setOnMarkerClickListener(this);
+
+    }
 }
